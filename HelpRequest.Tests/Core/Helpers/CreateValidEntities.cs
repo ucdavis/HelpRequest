d62923c9e4827d90472188dc16561d8c3d465be1
Sylvestre, Jason <jsylvestre@ucdavis.edu>
2010-08-16 15:22:11 +0000

    Added stuff for tests.
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/HelpRequest/Trunk/HelpRequest;C8013

---
+++ HelpRequest.Tests/Core/Helpers/CreateValidEntities.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HelpRequest.Core.Domain;
+
+namespace HelpRequest.Tests.Core.Helpers
+{
+    public static class CreateValidEntities
+    {
+        public static HelpTopic HelpTopic(int? counter)
+        {
+            var rtValue = new HelpTopic();
+            rtValue.Question = "Question" + counter.Extra();
+            rtValue.Answer = "Answer" + counter.Extra();
+            rtValue.AvailableToPublic = true;
+            return rtValue;
+        }
+
+        #region Helper Extension
+
+        private static string Extra(this int? counter)
+        {
+            var extraString = "";
+            if (counter != null)
+            {
+                extraString = counter.ToString();
+            }
+            return extraString;
+        }
+
+        #endregion Helper Extension
+    }
+}
